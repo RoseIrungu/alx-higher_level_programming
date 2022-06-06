27b2128fa06791e9f9f46b0b5bf38ba3be5c6df6
RoseIrungu <irungurosewangari@gmail.com>
2022-06-06 08:37:31 -0700

    no copying in c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = ''
-    if char != 'c' and char != 'C':
-        my_string += char
-        print(my_string)
+    my_string = my_string.translate({ord(i): None for i in 'Cc'})
+    return (my_string)
