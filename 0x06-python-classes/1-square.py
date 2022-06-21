22e8e25f0c8c5162df101bf3659919879b2e8535
RoseIrungu <irungurosewangari@gmail.com>
2022-06-21 13:15:49 +0300

    Square of size 3

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" creating as class called square."""
+
+
+class Square:
+    """ A class square with size as private arguements."""
+
+
+def __init__(self, size):
+    self.__size = size
