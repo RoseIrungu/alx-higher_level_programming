089689d8ddcf7997a5745f38a1eb275e7c2983b3
RoseIrungu <irungurosewangari@gmail.com>
2022-06-21 13:22:05 +0300

    task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" creating as class called square."""
+""" creating as class called square with attribute as size."""
 
 
 class Square:
-    """ A class square with size as private arguements."""
+    """ A class square with size as private instantce attribute."""
 
 
 def __init__(self, size):
