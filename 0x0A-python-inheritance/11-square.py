23f28d04f6935e83a93983c445bc263038b06745
RoseIrungu <irungurosewangari@gmail.com>
2022-07-04 17:20:40 +0300

    The Prints a square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+"""creating a new class Square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class sqaure inherits rectangle"""
+
+    def __init__(self, size):
+        """creating an instance of class rectangle"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ defining the area method"""
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """calling the string method"""
+
+        return f"[Square] {self.__size}/{self.__size}"
