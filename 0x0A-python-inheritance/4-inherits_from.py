173ed49a0a38edd9cef03f271ddbd66873e12916
RoseIrungu <irungurosewangari@gmail.com>
2022-07-04 13:48:41 +0300

    Only a sub class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+""" defining a function"""
+
+
+def inherits_from(obj, a_class):
+    if type(obj) != a_class and isinstance(obj, a_class):
+        return True
+    else:
+        return False
