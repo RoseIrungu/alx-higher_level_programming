1d58520640d78fca528f4808edd81c78eac8b1bf
RoseIrungu <irungurosewangari@gmail.com>
2022-07-04 17:53:17 +0300

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+"""new class """
+
+
+class MyInt(int):
+
+    """class int inherits items"""
+
+    def __eq__(self, other):
+        """returns false if its equal"""
+
+        return False
+
+    def __ne__(self, other):
+        """ returns False if they are not equal"""
+
+        return False
