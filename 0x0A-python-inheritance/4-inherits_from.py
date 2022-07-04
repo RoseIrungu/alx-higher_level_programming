9a4afe90a1178478cec7d731d5cc72c239d6680f
RoseIrungu <irungurosewangari@gmail.com>
2022-07-04 13:52:17 +0300

    Sub class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,6 +5,8 @@
 
 
 def inherits_from(obj, a_class):
+    """The fuction returns true if object is inherited"""
+
     if type(obj) != a_class and isinstance(obj, a_class):
         return True
     else:
