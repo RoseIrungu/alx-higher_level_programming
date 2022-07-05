723155552f1b94ef93a33d5de4fdaa6418cf9439
RoseIrungu <irungurosewangari@gmail.com>
2022-07-05 13:20:09 +0300

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+""" defining a json function"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """ function returns string literals"""
+
+    return json.dumps(my_obj)
