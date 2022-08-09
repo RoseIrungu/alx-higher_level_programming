55d2d46caaf66ca4b115fd63bb35877e8b4d82ae
RoseIrungu <irungurosewangari@gmail.com>
2022-08-09 23:43:54 +0300

    temperature

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- at displays the average temperature (Fahrenheit) by city 
+-- in descending order
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
