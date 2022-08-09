cbf6747997222406a95988a56804f9372ff3d3f6
RoseIrungu <irungurosewangari@gmail.com>
2022-08-09 18:20:29 +0300

    creating table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- database queries and mysql
 -- a script that creates a table with values
-CREATE TABLE IF EXISTS first_table(
+CREATE TABLE IF EXISTS first_table VALUE(
 	id = INT,
 	name = VARCHAR(256)
 );
