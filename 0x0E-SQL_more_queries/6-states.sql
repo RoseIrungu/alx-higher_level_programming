f058920aa81d5b8033278a5469bfe50ce414972b
RoseIrungu <irungurosewangari@gmail.com>
2022-08-10 21:06:41 +0300

    States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 -- script that creates the database hbtn_0d_usa
 -- create table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREEMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));;
