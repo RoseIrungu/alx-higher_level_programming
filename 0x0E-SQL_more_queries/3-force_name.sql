a6e0a5106f6acddf789697f617d48c2c6a95cfc1
RoseIrungu <irungurosewangari@gmail.com>
2022-08-10 17:10:05 +0300

    not empty

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,3 @@
+-- table with values
 -- creating a table
-CREATE TABLE IF NOT EXISTS force_name ('id' INT, 'name' VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS `force_name` (`id` INT, `name` VARCHAR(256) IS NOT NULL);
