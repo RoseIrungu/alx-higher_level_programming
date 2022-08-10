997592a737207dce001868e485b6115d1ab17f34
RoseIrungu <irungurosewangari@gmail.com>
2022-08-10 15:50:35 +0300

    privilege

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- Creating a new user and givin them privillages
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
--- giving privileges
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost` WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON * . * TO `user_0d_1`@`localhost`;
