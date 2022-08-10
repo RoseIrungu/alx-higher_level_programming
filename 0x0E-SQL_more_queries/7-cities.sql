e9c1fd3849e1b7ff427d3616d88a85d80acff20e
RoseIrungu <irungurosewangari@gmail.com>
2022-08-10 22:10:59 +0300

    primary and foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,4 @@ id INT UNIQUE AUTO_INCREEMENT NOT NULL,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
 PRIMARY KEY(id),
-FOREIGN KEY(state_id) REFERENCES states(id)
-);
+FOREIGN KEY(state_id) REFERENCES states(id));
